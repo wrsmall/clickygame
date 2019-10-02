2af9f9d70a0193ddb4d8f5b397b64af66135eeda
wrsmall <william.r.small89@gmail.com>
2019-10-01 22:05:51 -0400

    redeploying

---
+++ build/precache-manifest.c88b8816f2f2377882e9c6999057af2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b091b299efb5ff93ccc0ed784848f65",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "288d7b73a4da61fc9347",
+    "url": "/clickygame/static/css/main.693f7f61.chunk.css"
+  },
+  {
+    "revision": "40c2e7678a524532eb5f",
+    "url": "/clickygame/static/js/2.fefdd20e.chunk.js"
+  },
+  {
+    "revision": "288d7b73a4da61fc9347",
+    "url": "/clickygame/static/js/main.8388a5fb.chunk.js"
+  },
+  {
+    "revision": "1db345fc804afed442a0",
+    "url": "/clickygame/static/js/runtime-main.898fc85b.js"
+  }
+]);
